aab31614ef8d9220690a647c5096844abf3db3a8
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-07-24 10:56:05 -0500

    added button click events, added base variables

--- block24react/src/App.jsx
+++ block24react/src/App.jsx
@@ -3,16 +3,20 @@ import { puppyList } from './data.js'
 import { useState } from 'react';
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}> {puppy.name} </p>
+            return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}> {puppy.name} </p>
           })
         }
+        <p>{ featPupId && <p>{ featPupId }</p> }</p>
       </div>  
     </>
   )
